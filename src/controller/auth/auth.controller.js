9daa9b349d2494b4d07002ba9dbaee9b7c11fdb3
VazquezNicolas <nicolasorlandovazquez@gmail.com>
2023-05-04 00:51:15 -0300

    agrego forms

--- src/controller/auth/auth.controller.js
+++ src/controller/auth/auth.controller.js
@@ -4,18 +4,38 @@ const router = Router()
 
 router.post('/', async (req,res) => {
     try {
-        const{first_name, last_name, email, age, password} = req.body
-        const newUserInfo = {first_name, last_name, email, age, password}
+        const{email, password} = req.body
 
-        const user = await Users.create(newUserInfo)
-        console.log("hola")
+        const user = await Users.findOne({email })
+        if(!user) return res.status(400).json({
+            status: 'error',
+             error: 'El usuario y la constraseña no coincide'
+            })
         
-        res.status(201).json ({status: 'succes', message: user})
-    } catch (error) {
+        if(user.password !== password)
+        return res.status(400).json({
+            status: 'error',
+             error: 'El usuario y la constraseña no coincide'
+            })
 
+        req.session.user = {
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email
+        }
+
+        res.json ({status: 'succes', message: "Sesion iniciada"})
+    } catch (error) {
+        console.log(error)
         res.status(500).json ({status: 'error', error: 'Internal server error'})
     }
 })
 
+router.get('/logout', (req,res) => {
+    req.session.destroy (error => {
+        if(error) {return res.json({error})}
+        res.redirect('/login')
+    })
+})
 
 module.exports = router
--- src/controller/viewsTemplate/viewsTemplate.controller.js
+++ src/controller/viewsTemplate/viewsTemplate.controller.js
@@ -1,10 +1,20 @@
 const {Router} = require('express')
+const privateAccess = require('../../middlewares/privateAccess.middelwares')
+const publicAccess = require('../../middlewares/publicAccess.middlewares')
 
 const router = Router()
 
-router.get('/signup', (req,res) => {
-    
+router.get('/', privateAccess, (req,res) => {
+    const {user} = req.session
+    res.render('profile.handlebars', {user})
+})
+
+router.get('/signup', publicAccess, (req,res) => {  
     res.render('signUp.handlebars')
+})
+
+router.get('/login', publicAccess, (req,res) => {
+    res.render('login.handlebars')
 })
 
 module.exports = router
